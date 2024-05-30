eb12cd43e5218db8be473f4f5337974086f77515
Tanzeel <tanzeelmohammed21@gmail.com>
2024-05-30 12:15:03 +0530

    Module readsql created. Analysis

---
+++ readsql.py
@@ -0,0 +1,34 @@
+import os
+
+import pandas as pd
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv('HOST')
+username = os.getenv('USER')
+password = os.getenv('PASSWORD')
+
+connection = mysql.connector.connect(host=host,
+                                     user=username,
+                                     password=password,
+                                     database='swiftmarket')
+
+cursor = connection.cursor()
+
+def queryToDataFrame(query):
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
+
+
+
+
+def describeTable(tablename):
+    query = f"""describe {tablename};"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
